85e0e5db08b85e71fc6a0c629457ddd62badef55
Ida Hellqvist <ida@pop-os.localdomain>
2023-09-07 09:49:30 +0200

    Implement reading file and converting data to bodies

---
+++ main.py
@@ -0,0 +1,43 @@
+import struct
+from collections import namedtuple
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+Body = namedtuple("Body", ["x_pos", "y_pos", "mass", "x_vel", "y_vel", "brightness"])
+
+def get_data(file_name):
+    # Open the binary file in binary read mode
+    with open(file_name, 'rb') as file:
+        # Read the binary data from the file
+        binary_data = file.read()
+    # Calculate the number of double-precision floats in the binary data
+    num_doubles = len(binary_data) // 8  # Assuming each double is 8 bytes
+
+    # Unpack the binary data into a list of floats
+    float_list = struct.unpack('d' * num_doubles, binary_data)
+
+    # Return list of floats
+    return float_list
+
+def get_bodies(data):
+    bodies = []
+    i = 0
+    while (i < len(data)):
+        body = Body(x_pos=data[i], y_pos=data[i+1], mass=data[i+2], x_vel=data[i+3], y_vel=data[i+4], brightness=data[i+5])
+        bodies.append(body)
+        i+=6
+    return bodies
+        
+
+
+# Check that reading the file works
+input = get_data('input_data/circles_N_4.gal')
+print (input)
+
+# Check that creating bodies work 
+bodies = get_bodies(input)
+print(bodies)
+
+
+
+  
