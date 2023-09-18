41821b4b9961c7393248d9bc4102e0bcb57de3c8
dali0342 <david@DESKTOP-F960LHS>
2023-09-18 17:11:08 +0200

    Commented and cleared code

--- main.py
+++ main.py
@@ -1,211 +1,119 @@
-import struct
-from collections import namedtuple
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.animation import FuncAnimation
-import math
 
+TIMESTEP = 0.00001  # 10 ** (-5), given in assignment
+EPSILON = 0.001  # 10 ** (-3), given in assignment
+NUMOFCALC = 200 # Decides number of calculations
 
-Body = namedtuple("Body", ["x_pos", "y_pos", "mass", "x_vel", "y_vel", "brightness"])
-TIMESTEP = 10**(-5)
-TIME = 200
-MACHINE_EPSILON = 10**(-3)
+# Kör rad nedan för att välja datafil. Kolla att pathen stämmer.
+# För referens kör rad nedan
+# ./compare_gal_files/compare_gal_files ANTAL_BODIES REFERENSFIL output.gal
 
-def get_data(file_name):
-    """
-    Get binary data from file and convert to list of double precision floats.
+# Get binary data from fil
+data = np.fromfile("input_data\ellipse_N_00010.gal", dtype=float, ) # pos_maxdiff = 0.026866764988
+# data = np.fromfile("input_data\ellipse_N_00100.gal", dtype=float, ) # pos_maxdiff = 0.021573955637
+# data = np.fromfile("input_data\circles_N_2.gal", dtype=float, ) # ej ref data, endast för oservation av animation
+# data = np.fromfile("input_data\sun_and_planets_N_4.gal", dtype=float, ) # ej ref data, endast för oservation av animation
 
-    Parameters:
-    file_name (string): Name of the file containing binary data.
+N = int(len(data) / 6) # Number of bodies
+G = 100 / N # Gravitational constant
 
-    Returns:
-    list: List of data as floats.
-    """
+# Groups data into arrays corresponding to what the data represents
+data = data.reshape(-1, 6)
 
-    # Open the binary file 
-    with open(file_name, 'rb') as file:
-        binary_data = file.read()
+# Allocate space for all data points that will be generated
+x_pos_history = np.zeros((NUMOFCALC, N))
+y_pos_history = np.zeros((NUMOFCALC, N))
+x_vel_history = np.zeros((NUMOFCALC, N))
+y_vel_history = np.zeros((NUMOFCALC, N))
 
-    # Calculate the number of double-precision floats in the binary data, assuming each double is 8 bytes
-    num_doubles = len(binary_data) // 8  
+# Extract data to separate arrays
+# Mass and brightness are never manipulated
+x_pos_history[0] = data[:, 0]
+y_pos_history[0] = data[:, 1]
+mass = data[:, 2]
+x_vel_history[0] = data[:, 3]
+y_vel_history[0] = data[:, 4]
+brightness = data[:, 5]
 
-    # Unpack the binary data into a list of floats
-    float_list = struct.unpack('d' * num_doubles, binary_data)
+# Calculate force exerted on one particle
+def calculate_force(time_step, current):
+    sum_force_x = 0
+    sum_force_y = 0
 
-    return float_list
-
-def convert_data_to_bodies(data):
-    """
-    Convert data to a list of tuples, where each tuple is a Body.
-
-    Parameters:
-    data (list): List of floats representing body data. The order of numbers in list 
-    is as follows:
-        body 0 position x
-        body 0 particle y
-        body 0 mass
-        body 0 velocity x
-        body 0 velocity y
-        body 1 brightness
-        body 1 position x
-        body 1 particle y
-        body 1 mass
-        ...
-
-    Returns:
-    list: List containing multiple instances of Body.
-    """
-
-    bodies = []
-    i = 0
-
-    while (i < len(data)):
-        body = Body(x_pos = data[i], y_pos = data[i + 1], mass = data[i + 2], x_vel = data[i + 3], y_vel = data[i + 4], brightness = data[i + 5])
-        bodies.append(body)
-        i += 6
-
-    return bodies
-
-def calculate_force(body_i, i, bodies): 
-    G = 100 / len(bodies)
-    sum_x = 0
-    sum_y = 0
-
-    for j, body_j in enumerate(bodies):
-        if j == i: # Skip calculation for the body itself
+    for i in range(N-1):
+        if i == current:
             continue
 
-        dx = body_j.x_pos - body_i.x_pos
-        dy = body_j.y_pos - body_i.y_pos
+        dx = x_pos_history[time_step-1][current] - x_pos_history[time_step -1][i]
+        dy = y_pos_history[time_step-1][current] - y_pos_history[time_step-1][i]
 
-        r_squared = dx ** 2 + dy ** 2 
-        r = math.sqrt(r_squared)
-        denominator = (r + MACHINE_EPSILON) ** 3
-        
-        force_x = (G * body_i.mass * body_j.mass / denominator) * dx
-        force_y = (G * body_i.mass * body_j.mass / denominator) * dy
+        r_squared = dx ** 2 + dy ** 2
+        r = np.sqrt(r_squared)
+        denominator = (r + EPSILON) ** 3
 
-        sum_x += force_x
-        sum_y += force_y
+        force_x = (mass[i] / denominator) * dx
+        force_y = (mass[i] / denominator) * dy
 
-    return sum_x, sum_y
+        sum_force_x += force_x
+        sum_force_y += force_y
+    sum_force_x = -G * mass[current] * sum_force_x
+    sum_force_y = -G * mass[current] * sum_force_y
+    return sum_force_x, sum_force_y
 
-def update_position(body, index, bodies):
-    ''''
-    body (Body): The body to update position of.
-    bodies (list of Body): All of the bodies.
-
-    Returns:
-    Body: A new instance of Body, with the same mass and brightness as
-    body but with updated velocity and position.
-    '''
-    force_x, force_y  = calculate_force(body, index, bodies) 
-    acceleration_x = force_x / body.mass
-    acceleration_y = force_y / body.mass
+# Update position of one particle
+def update_position(time_step, current):
+    sum_force_x, sum_force_y = calculate_force(time_step, current)
     
-    # New velocity
-    new_vel_x = body.x_vel + TIMESTEP * acceleration_x
-    new_vel_y = body.y_vel + TIMESTEP * acceleration_y
+    # acceleration
+    acceleration_x = sum_force_x / mass[current]
+    acceleration_y = sum_force_y / mass[current]
 
-    # New position
-    new_pos_x = body.x_pos + TIMESTEP * new_vel_x
-    new_pos_y = body.y_pos + TIMESTEP * new_vel_y
-    
-    # Create new Body with same mass and brightness as body, but with new velocity and new position
-    new_body = Body(x_pos = new_pos_x, y_pos = new_pos_y, mass = body.mass, x_vel = new_vel_x, y_vel = new_vel_y, brightness = body.brightness)
-    # print(f"{'Old body: '} x_pos={body.x_pos}, y_pos={body.y_pos}, x_vel={body.x_vel}, y_vel={body.y_vel} ")
-    # print(f"{'New body: '} x_pos={new_body.x_pos}, y_pos={new_body.y_pos}, x_vel={new_body.x_vel}, y_vel={new_body.y_vel} ")
-    #print()
-    return new_body
-    
-    
-def update_all_positions(bodies): 
-    '''
-    Update the position of all bodies.
+    # new velocity
+    x_vel_history[time_step][current] = x_vel_history[time_step-1][current] + TIMESTEP * acceleration_x
+    y_vel_history[time_step][current] = y_vel_history[time_step-1][current] + TIMESTEP * acceleration_y
 
-    Parameters:
-    bodies (list): List of the bodies to update.
+    # new position
+    x_pos_history[time_step][current] = x_pos_history[time_step-1][current] + TIMESTEP * x_vel_history[time_step][current]
+    y_pos_history[time_step][current] = y_pos_history[time_step-1][current] + TIMESTEP * y_vel_history[time_step][current]
 
-    Return:
-    list: List containing bodies with updated positions. 
-    '''
-    new_bodies = []
+ # Make all calculations 
+def main_sim():
+    for time_step in range(1, NUMOFCALC):
+        for current in range(N):
+            update_position(time_step, current)
 
-    for index, body in enumerate(bodies):
-        new_bodies.append(update_position(body, index, bodies))
+main_sim()
 
-    #print(new_bodies)
-    return new_bodies
+# Put data of last time step in the same order as input
+data = np.ravel(data)
+for i in range(0, N*6, 6):
+    data[i]   = (x_pos_history[NUMOFCALC-1][i//6])
+    data[i+1] = (y_pos_history[NUMOFCALC-1][i//6])
+    data[i+2] = (mass[i//6])
+    data[i+3] = (x_vel_history[NUMOFCALC-1][i//6])
+    data[i+4] = (y_vel_history[NUMOFCALC-1][i//6])
+    data[i+5] = (brightness[i//6])
 
-#bodies = convert_data_to_bodies(get_data('input_data/ellipse_N_00010.gal'))
 
+# Write data for last time step to file
+data.tofile("output.gal")
+
+# Animate 
 fig, ax = plt.subplots()
 
-# Define a list to store the bodies
-bodies_container = [convert_data_to_bodies(get_data('input_data/ellipse_N_02000.gal'))]
-
-def animate(frame, bodies_container):
-    # Update the positions of the bodies
-    bodies = bodies_container[0]
-    bodies = update_all_positions(bodies)
-    bodies_container[0] = bodies
-
-    # Clear the axis and plot the updated positions
+def animate(frame, x_pos_history, y_pos_history):
     ax.clear()
-    x_positions = [body.x_pos for body in bodies]
-    y_positions = [body.y_pos for body in bodies]
-    ax.plot(x_positions, y_positions, 'o', markersize=5, label='Bodies')
+    ax.scatter(x_pos_history[frame], y_pos_history[frame], s=1)
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.plot(x_pos_history[frame], y_pos_history[frame], 'o', markersize=3)
+    ax.set_title(f"Frame {frame+1}")
 
-     # Set fixed limits for the x and y axes (adjust these values as needed)
-    ax.set_xlim(0, 1.1)  # Set x-axis limits
-    ax.set_ylim(0.25, 0.7)  # Set y-axis limits
-    
-    # Customize your plot here (titles, labels, legends, etc.)
-    ax.set_title(f'Time Step {frame + 1}')
-    ax.legend()
-
-# Create an animation with a frame for each time step
-animation = FuncAnimation(fig, animate, frames=TIME, fargs=(bodies_container,), interval=0.5, repeat=False)  # 'TIME' is the total number of time steps
-
-# Show the animation
+# Create the animation, comment this out if you do not want an animation
+anim = FuncAnimation(fig, animate, frames=NUMOFCALC, fargs=(x_pos_history, y_pos_history), interval=0.01, repeat=False)
 plt.show()
 
-def write_bodies_to_gal(bodies, output_filename):
-    with open(output_filename, 'wb') as file:
-        for body in bodies:
-            file.write(struct.pack('d', body.x_pos))
-            file.write(struct.pack('d', body.y_pos))
-            
-            # Write mass
-            file.write(struct.pack('d', body.mass))
-            
-            # Write velocity
-            file.write(struct.pack('d', body.x_vel))
-            file.write(struct.pack('d', body.y_vel))
-            
-            # Write brightness
-            file.write(struct.pack('d', body.brightness))
-            print(body.x_pos)
-            print(body.y_pos)
-            print(body.mass)
-            print(body.x_vel)
-            print(body.y_vel)
-            print(body.brightness)
-            print()
-
-write_bodies_to_gal(bodies_container[0], 'output.gal')
-"""
-def animate(frame, bodies):
-    updated_bodies = update_all_positions(bodies)
-    x_positions = [body.x_pos for body in updated_bodies]
-    y_positions = [body.y_pos for body in updated_bodies]
-    ax.clear()
-    ax.plot(x_positions, y_positions, 'o', markersize=3, label='Bodies')
-    return updated_bodies
 
 
-
-fig, ax = plt.subplots()
-animation = FuncAnimation(fig, animate, frames=10, fargs = (bodies,), interval=1000)
-plt.show()
-
-"""
