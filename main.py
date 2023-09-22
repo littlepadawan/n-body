837e6e599b0d81d17703b98743eeb020d7e36b25
dali0342 <david@DESKTOP-F960LHS>
2023-09-22 12:04:04 +0200

    Added readability, completed version. 0.0 wrongs

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from matplotlib.animation import FuncAnimation
 
 TIMESTEP = 0.00001  # 10 ** (-5), given in assignment
 EPSILON = 0.001  # 10 ** (-3), given in assignment
-NUMOFCALC = 200 # Decides number of calculations
+NUMOFCALC = 201 # Decides number of calculations
 
 # Kör rad nedan för att välja datafil. Kolla att pathen stämmer.
 # För referens kör rad nedan
 # ./compare_gal_files/compare_gal_files ANTAL_BODIES REFERENSFIL output.gal
 
 # Get binary data from fil
+
 data = np.fromfile("input_data\ellipse_N_00010.gal", dtype=float, ) # pos_maxdiff = 0.026866764988
 # data = np.fromfile("input_data\ellipse_N_00100.gal", dtype=float, ) # pos_maxdiff = 0.021573955637
 # data = np.fromfile("input_data\circles_N_2.gal", dtype=float, ) # ej ref data, endast för oservation av animation
-# data = np.fromfile("input_data\sun_and_planets_N_4.gal", dtype=float, ) # ej ref data, endast för oservation av animation
+#data = np.fromfile("input_data\sun_and_planets_N_4.gal", dtype=float, ) # ej ref data, endast för oservation av animation
 
 N = int(len(data) / 6) # Number of bodies
 G = 100 / N # Gravitational constant
@@ -32,21 +33,21 @@ y_vel_history = np.zeros((NUMOFCALC, N))
 # Mass and brightness are never manipulated
 x_pos_history[0] = data[:, 0]
 y_pos_history[0] = data[:, 1]
-mass = data[:, 2]
+mass             = data[:, 2]
 x_vel_history[0] = data[:, 3]
 y_vel_history[0] = data[:, 4]
-brightness = data[:, 5]
+brightness       = data[:, 5]
 
 # Calculate force exerted on one particle
 def calculate_force(time_step, current):
     sum_force_x = 0
     sum_force_y = 0
 
-    for i in range(N-1):
+    for i in range(N):
         if i == current:
             continue
 
-        dx = x_pos_history[time_step-1][current] - x_pos_history[time_step -1][i]
+        dx = x_pos_history[time_step-1][current] - x_pos_history[time_step-1][i]
         dy = y_pos_history[time_step-1][current] - y_pos_history[time_step-1][i]
 
         r_squared = dx ** 2 + dy ** 2
@@ -58,6 +59,7 @@ def calculate_force(time_step, current):
 
         sum_force_x += force_x
         sum_force_y += force_y
+
     sum_force_x = -G * mass[current] * sum_force_x
     sum_force_y = -G * mass[current] * sum_force_y
     return sum_force_x, sum_force_y
@@ -71,12 +73,12 @@ def update_position(time_step, current):
     acceleration_y = sum_force_y / mass[current]
 
     # new velocity
-    x_vel_history[time_step][current] = x_vel_history[time_step-1][current] + TIMESTEP * acceleration_x
-    y_vel_history[time_step][current] = y_vel_history[time_step-1][current] + TIMESTEP * acceleration_y
+    x_vel_history[time_step][current] = x_vel_history[time_step-1][current] + (TIMESTEP * acceleration_x)
+    y_vel_history[time_step][current] = y_vel_history[time_step-1][current] + (TIMESTEP * acceleration_y)
 
     # new position
-    x_pos_history[time_step][current] = x_pos_history[time_step-1][current] + TIMESTEP * x_vel_history[time_step][current]
-    y_pos_history[time_step][current] = y_pos_history[time_step-1][current] + TIMESTEP * y_vel_history[time_step][current]
+    x_pos_history[time_step][current] = x_pos_history[time_step-1][current] + (TIMESTEP * x_vel_history[time_step][current])
+    y_pos_history[time_step][current] = y_pos_history[time_step-1][current] + (TIMESTEP * y_vel_history[time_step][current])
 
  # Make all calculations 
 def main_sim():
@@ -86,19 +88,20 @@ def main_sim():
 
 main_sim()
 
-# Put data of last time step in the same order as input
-data = np.ravel(data)
-for i in range(0, N*6, 6):
-    data[i]   = (x_pos_history[NUMOFCALC-1][i//6])
-    data[i+1] = (y_pos_history[NUMOFCALC-1][i//6])
-    data[i+2] = (mass[i//6])
-    data[i+3] = (x_vel_history[NUMOFCALC-1][i//6])
-    data[i+4] = (y_vel_history[NUMOFCALC-1][i//6])
-    data[i+5] = (brightness[i//6])
+# Put data of last time step in the same order as
+output = np.zeros(N*6)
+for i in range(0, N, 1):
+    output[i*6]   = (x_pos_history[NUMOFCALC-1][i])
+    output[i*6+1] = (y_pos_history[NUMOFCALC-1][i])
+    output[i*6+2] = (mass[i])
+    output[i*6+3] = (x_vel_history[NUMOFCALC-1][i])
+    output[i*6+4] = (y_vel_history[NUMOFCALC-1][i])
+    output[i*6+5] = (brightness[i])
 
 
+#print(output)
 # Write data for last time step to file
-data.tofile("output.gal")
+output.tofile("output.gal")
 
 # Animate 
 fig, ax = plt.subplots()
@@ -109,7 +112,7 @@ def animate(frame, x_pos_history, y_pos_history):
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     ax.plot(x_pos_history[frame], y_pos_history[frame], 'o', markersize=3)
-    ax.set_title(f"Frame {frame+1}")
+    ax.set_title(f"Frame {frame}")
 
 # Create the animation, comment this out if you do not want an animation
 anim = FuncAnimation(fig, animate, frames=NUMOFCALC, fargs=(x_pos_history, y_pos_history), interval=0.01, repeat=False)
